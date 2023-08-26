e8852c149c70e8f625f817bb423e3cef2e28af99
Julius J <bluestek@gmail.com>
2023-08-25 18:42:44 -0700

    Updated VacuumEntity to StateVacuumEntity

--- custom_components/proscenic/vacuum.py
+++ custom_components/proscenic/vacuum.py
@@ -7,7 +7,7 @@ from typing import Any
 
 import async_timeout
 
-from homeassistant.components.vacuum import VacuumEntity, VacuumEntityFeature
+from homeassistant.components.vacuum import StateVacuumEntity, VacuumEntityFeature
 from homeassistant.core import HomeAssistant, callback
 from homeassistant.helpers.entity_platform import AddEntitiesCallback
 from homeassistant.helpers.icon import icon_for_battery_level
@@ -28,21 +28,19 @@ async def async_setup_entry(
     vacuum = [ProscenicVacuum(config['device'])]
     async_add_entities(vacuum, update_before_add=True)
 
-class ProscenicVacuum(VacuumEntity):
+class ProscenicVacuum(StateVacuumEntity):
     """Ecovacs Vacuums such as Deebot."""
     _attr_should_poll = False
     _attr_fan_speed_list = ['quiet', 'auto', 'strong']
     _attr_fan_speed = 'auto'
     _attr_supported_features = (
-        VacuumEntityFeature.TURN_ON
-        | VacuumEntityFeature.TURN_OFF
+        VacuumEntityFeature.START
         | VacuumEntityFeature.PAUSE
         | VacuumEntityFeature.STOP
         | VacuumEntityFeature.RETURN_HOME
         | VacuumEntityFeature.FAN_SPEED
         | VacuumEntityFeature.BATTERY
-        | VacuumEntityFeature.STATUS
-        | VacuumEntityFeature.START
+        | VacuumEntityFeature.SEND_COMMAND
     )
 
     def __init__(self, proscenic_home) -> None:
@@ -110,6 +108,14 @@ class ProscenicVacuum(VacuumEntity):
         """Return the fan speed of the vacuum cleaner."""
         return super().fan_speed
 
+    async def async_pause(self, **kwargs: Any) -> None:
+        """Pause the vacuum cleaner."""
+        await self.vacuum.pause_cleaning()
+
+    async def async_return_to_base(self, **kwargs: Any) -> None:
+        """Stop the vacuum cleaner."""
+        await self.vacuum.return_to_dock()
+
     async def async_set_fan_speed(self, fan_speed: str, **kwargs: Any) -> None:
         """Set fan speed."""
         await self.vacuum.proscenic_powermode(fan_speed)
@@ -121,32 +127,9 @@ class ProscenicVacuum(VacuumEntity):
         else:
             await self.vacuum.start_clean()
 
-    async def async_turn_on(self, **kwargs: Any) -> None:
-        """Turn the vacuum on and start cleaning."""
-        await self.async_start()
-
-    async def async_pause(self, **kwargs: Any) -> None:
-        """Pause the vacuum cleaner."""
-        await self.vacuum.pause_cleaning()
-
     async def async_stop(self, **kwargs: Any) -> None:
         """Stop the vacuum cleaner."""
         await self.async_pause()
-
-    async def async_start_pause(self, **kwargs: Any) -> None:
-        mode = self._attr_status
-        if mode == None or mode == '' or mode == 'pause':
-            await self.async_start()
-        elif mode == 'sweep':
-            await self.async_pause()
-
-    async def async_return_to_base(self, **kwargs: Any) -> None:
-        """Stop the vacuum cleaner."""
-        await self.vacuum.return_to_dock()
-
-    async def async_turn_off(self, **kwargs: Any) -> None:
-        """Turn the vacuum off stopping the cleaning and returning home."""
-        await self.async_return_to_base()
 
     async def async_send_command(
         self,
